3ae9a34e373bfcb88c911d25f4b8867189b2cdce
罗洪来 <honglai.luo@dreamsmart.com>
2026-03-23 10:15:24 +0800

    bugfix: tar restore by pipe

--- io-tar/src/main/kotlin/tiiehenry/android/compress/zstd/TarJNI.kt
+++ io-tar/src/main/kotlin/tiiehenry/android/compress/zstd/TarJNI.kt
@@ -6,5 +6,8 @@ object TarJNI {
         System.loadLibrary("tar-jni")
     }
 
-    external fun callCli(pipeFile: String,stdOut: String, stdErr: String, argv: Array<String>): Int
+    /**
+     * @param pipeFile 管道文件，注意不能直接在参数中指定管道文件，只能将管道文件作为stdin输入，pipeFile会重定向到stdin
+     */
+    external fun callCli(pipeFile: String, stdOut: String, stdErr: String, argv: Array<String>): Int
 }
